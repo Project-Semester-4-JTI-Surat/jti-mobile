a589ffad82ffdd365a6992392958124b89b44429
Akbar Ramadhani Firdaus <54605434+arfphp@users.noreply.github.com>
2023-04-23 16:46:19 +0700

    Perbaikan Layout Tambah Surat (1)

--- app/src/main/java/com/example/projectsemester4/TambahSurat.java
+++ app/src/main/java/com/example/projectsemester4/TambahSurat.java
@@ -1,20 +1,37 @@
 package com.example.projectsemester4;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TambahSurat extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    EditText kepada, alamat, tanggal;
-    Spinner spJenisSurat, spNamaDosen, kebutuhan;
-    TextView judulSurat, jsSurat, nmDosen, kpd, almt, tgl, kbth;
+    private EditText kepada, alamat, tanggal;
+    private Spinner spJenisSurat, spNamaDosen, kebutuhan;
+    private TextView judulSurat, jsSurat, nmDosen, kpd, almt, tgl, kbth;
+    private RadioGroup pilihan;
+    private RadioButton pilihan1, pilihan2;
+    private TextView anggotaKe, txNim, txNamaAnggota, txProdi, txTlpAnggota;
+    private EditText nimAnggota, namaAnggota, tlpAnggota;
+    private Spinner prodiAnggota;
+    private Button tambahAnggotaButton, resetFormButton, ajukanButton;
+    private int jumlahAnggota = 1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tambah_surat);
@@ -33,6 +50,21 @@ public class TambahSurat extends AppCompatActivity implements AdapterView.OnItem
         almt = findViewById(R.id.almt);
         tgl = findViewById(R.id.tgl);
         kbth = findViewById(R.id.kbth);
+
+        pilihan = findViewById(R.id.pilihan);
+        pilihan1 = findViewById(R.id.pilihan1);
+        pilihan2 = findViewById(R.id.pilihan2);
+        anggotaKe = findViewById(R.id.anggotaKe);
+        txNim = findViewById(R.id.txNim);
+        txNamaAnggota = findViewById(R.id.txNamaAnggota);
+        txProdi = findViewById(R.id.txProdi);
+        txTlpAnggota = findViewById(R.id.txTlpAnggota);
+        nimAnggota = findViewById(R.id.nimAnggota);
+        namaAnggota = findViewById(R.id.namaAnggota);
+        tlpAnggota = findViewById(R.id.tlpAnggota);
+        tambahAnggotaButton = findViewById(R.id.tambahAnggotaButton);
+        resetFormButton = findViewById(R.id.resetButton);
+        ajukanButton = findViewById(R.id.ajukanButton);
 
         ArrayAdapter<CharSequence> adapterJS = ArrayAdapter.createFromResource(this,
                 R.array.jenis_surat, android.R.layout.simple_spinner_item);
@@ -53,6 +85,65 @@ public class TambahSurat extends AppCompatActivity implements AdapterView.OnItem
         spNamaDosen.setOnItemSelectedListener(this);
         kebutuhan.setOnItemSelectedListener(this);
 
+        tambahAnggotaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mengaktifkan RadioButton pilihan2 dan menonaktifkan pilihan1
+                pilihan1.setEnabled(false);
+                pilihan2.setEnabled(true);
+
+                // Menambahkan tampilan form anggota baru
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.tambah_surat, null);
+                LinearLayout anggotaLayout = findViewById(R.id.LinearLayout2);
+                anggotaLayout.addView(view, anggotaLayout.getChildCount());
+
+                // Menambahkan nomor urut anggota
+                jumlahAnggota++;
+                TextView anggotaKe = findViewById(R.id.anggotaKe);
+                anggotaKe.setVisibility(View.VISIBLE);
+                anggotaKe.setText("Anggota Ke-" + jumlahAnggota);
+
+
+                // Mengambil referensi dari elemen form anggota yang baru ditambahkan
+                TextView txNim = view.findViewById(R.id.txNim);
+                TextView txNamaAnggota = view.findViewById(R.id.txNamaAnggota);
+                TextView txProdi = view.findViewById(R.id.txProdi);
+                TextView txTlpAnggota = view.findViewById(R.id.txTlpAnggota);
+                EditText nimAnggota = view.findViewById(R.id.nimAnggota);
+                EditText namaAnggota = view.findViewById(R.id.namaAnggota);
+                EditText tlpAnggota = view.findViewById(R.id.tlpAnggota);
+                Spinner prodiAnggota = view.findViewById(R.id.prodiAnggota);
+
+                // Mengatur adapter untuk Spinner prodiAnggotaBaru
+                ArrayAdapter<CharSequence> adapterProdi = ArrayAdapter.createFromResource(TambahSurat.this,
+                        R.array.prodi, android.R.layout.simple_spinner_item);
+                adapterProdi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                prodiAnggota.setAdapter(adapterProdi);
+            }
+        });
+
+
+        resetFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // mereset semua field ke nilai awal
+                kepada.setText("");
+                alamat.setText("");
+                tanggal.setText("");
+                spJenisSurat.setSelection(0);
+                spNamaDosen.setSelection(0);
+                kebutuhan.setSelection(0);
+                pilihan1.setChecked(true);
+                pilihan1.setEnabled(true);
+                LinearLayout layout = findViewById(R.id.LinearLayout2);
+                int count = layout.getChildCount();
+                for (int i = count - 1; i >= 5; i--) {
+                    View view = layout.getChildAt(i);
+                    layout.removeView(view);
+                }
+            }
+        });
     }
 
     @Override
